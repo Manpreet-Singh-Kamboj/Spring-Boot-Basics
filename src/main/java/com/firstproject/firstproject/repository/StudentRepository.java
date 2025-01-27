6e80021699daa9d855c88850d87529286ebcc841
Manpreet Singh <97432223+Manpreet3033@users.noreply.github.com>
2025-01-27 14:56:23 +0530

    Learned about DTO(Data Transfer Objects) and Implemented them

--- src/main/java/com/firstproject/firstproject/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.firstproject.firstproject.repository;
-
-import com.firstproject.firstproject.entities.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,String> {
-}
--- src/test/java/com/blogapp/FirstprojectApplicationTests.java
+++ src/test/java/com/blogapp/FirstprojectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.firstproject.firstproject;
+package com.blogapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
