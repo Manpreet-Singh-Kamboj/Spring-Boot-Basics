b9b76b60b150a19914fba26f0c8c6a3d1a4caf34
Manpreet Singh <97432223+Manpreet3033@users.noreply.github.com>
2025-01-29 19:31:32 +0530

    Added Post creation functionality and get user posts and get posts by category id

--- src/main/java/com/blogapp/repository/PostRepository.java
+++ src/main/java/com/blogapp/repository/PostRepository.java
@@ -4,9 +4,11 @@ import com.blogapp.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post,String> {
     @Query(value = "SELECT p from Post AS p INNER JOIN categories AS c WHERE c.id = :categoryId")
     List<Post> findAllByCategoryId(@Param("categoryId") String categoryId);
--- src/main/java/com/blogapp/repository/UserRepository.java
+++ src/main/java/com/blogapp/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.blogapp.repository;
 
 import com.blogapp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,String> {
     boolean existsByEmail(String email);
 }
